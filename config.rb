d38647491ac047effe629ce1f42491ff248bb222
Tito Bottitta <tito@upstatement.com>
2013-11-14 18:32:35 -0500

    Initial commit of entire site

---
+++ config.rb
@@ -0,0 +1,13 @@
+http_path = "/"
+css_dir = "css"
+css_path = "css"
+sass_dir = "sass"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+images_dir = "img"
+
+line_comments = false
+output_style = :compressed
+#:compressed :expanded
+
+add_import_path "components/ups-sass-patterns"
